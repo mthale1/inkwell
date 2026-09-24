c6263f88d37328654b59fb3800be4fd303f613fd
mthale1 <mthale1@crimson.ua.edu>
2026-09-24 15:31:16 -0500

    feat: add Inkwell navigation icon

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -9,7 +9,10 @@ import { NavLink } from "react-router-dom";
 export function NavBar() {
     return (
         <nav className="flex items-center justify-between px-4 py-3 border-b border-gray-200">
-            <span className="font-bold text-lg">Inkwell</span>
+            <div className="flex items-center gap-2">
+                <img src="/favicon.svg" alt="" className="w-6 h-6" />
+                <span className="font-bold text-lg">Inkwell</span>
+            </div>
 
             <div className="flex gap-4">
                 <NavLink
